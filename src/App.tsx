0e4ac42671b7a1393f014d2b1f6d13b3e2f4a9c0
AbderrahimHn06 <haiounabderrahim06@gmail.com>
2025-12-25 07:55:28 +0100

    Completed: set up the theme provider (css)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Index from "./Routes/Index";
+import Login from "./Routes/Login";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/chat" element={<Index />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
