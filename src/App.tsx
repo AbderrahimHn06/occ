1f166377dec5cf82024eada20c6ab2d2068ed25c
AbderrahimHn06 <haiounabderrahim06@gmail.com>
2026-01-17 10:12:11 +0100

    Fixing Routing To the Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/chat" element={<Index />} />
+        <Route path="/" element={<Index />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </>
